ccbec02be9bf670e34beecc14e5ea90508a4e86b
jekettun <jereatkettunen@fmail.com>
2025-04-21 11:44:10 +0300

    up to 4.12

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -10,6 +10,14 @@ blogsRouter.get('/', async(request, response) => {
 blogsRouter.post('/', async(request, response) => {
   const blog = new Blog(request.body)
 
+  if (!blog.title || !blog.url) {
+    return response.status(400).json({ error: 'title or url missing' })
+  }
+
+  if (blog.likes === undefined) {
+    blog.likes = 0
+  }
+
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
 
--- part4/blogList/tests/blog_api.test.js
+++ part4/blogList/tests/blog_api.test.js
@@ -6,6 +6,7 @@ const app = require('../app')
 const helper = require('./test_helper')
 const Blog = require('../models/blog')
 
+
 const api = supertest(app)
 
 beforeEach(async () => {
@@ -46,6 +47,56 @@ test('a valid blog can be added ', async () => {
 
   assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
   assert(titles.includes('Test title'))
+})
+
+test('a blog without likes defaults to 0', async () => {
+  const newBlog = {
+    title: 'Likes are not required',
+    author: 'Like McLover',
+    url: 'https://NoLikesBlog.com/',
+    __v: 0
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  const response = await api.get('/api/blogs')
+  const addedBlog = response.body.find(blog => blog.title === 'Likes are not required')
+
+  assert.strictEqual(addedBlog.likes, 0)
+})
+
+test('a blog without title and url is not added', async () => {
+  const newBlogNoTitle = {
+    url: 'https://NoTitleBlog.com/',
+    likes: 0,
+    __v: 0
+  }
+  const newBlogNoUrl = {
+    title: 'No URL',
+    likes: 0,
+    __v: 0
+  }
+  const newBlogNoTitleAndUrl = {
+    likes: 0,
+    __v: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlogNoTitle)
+    .expect(400)
+
+  await api
+    .post('/api/blogs')
+    .send(newBlogNoUrl)
+    .expect(400)
+
+  await api
+    .post('/api/blogs')
+    .send(newBlogNoTitleAndUrl)
+    .expect(400)
 })
 
 after(async () => {
