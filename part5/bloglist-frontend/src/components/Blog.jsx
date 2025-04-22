73f960c182c4780e7ae3e3fe847de8abf4f3269b
jekettun <jereatkettunen@fmail.com>
2025-04-22 10:46:30 +0300

    5.7 with states

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,6 +1,8 @@
-import Togglable from "./Togglable"
+import { useState} from 'react'
 
 const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -8,23 +10,24 @@ const Blog = ({ blog }) => {
     borderWidth: 1,
     marginBottom: 5
   }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
   
   return(
   <div style = {blogStyle} className='blog'>
-    {blog.title} {blog.author}
-    <Togglable buttonOpenLabel='view' buttonCloseLabel='hide'>
-      <div>
-        {blog.url}
-      </div>
-      <div>
-        likes {blog.likes + "  "}  
-        <button className='like-button'>Like</button>
-      </div>
-      <div>
-        {blog.user.name}
-      </div>
-    </Togglable>
-  </div>  
+    {blog.title} {blog.author} {' '}
+    <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+    <div style={{ display: visible ? '' : 'none' }}>
+      <p>{blog.url}</p>
+      <p>{blog.likes} {'likes '}
+        <button>Like</button>
+      </p>
+      <p>added by {blog.user.name}</p>
+      <button>remove</button>
+    </div>
+  </div>
 )}
 
 export default Blog
