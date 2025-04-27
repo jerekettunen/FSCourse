4622ffa6e71d4073afdfe8ba46a03cd4d636ca02
jekettun <jereatkettunen@fmail.com>
2025-04-27 14:40:01 +0300

    up to 9.5

--- part9/initEx/bmiCalculator.ts
+++ part9/initEx/bmiCalculator.ts
@@ -2,19 +2,6 @@ interface BmiValues {
   height: number
   weight: number
 }
-const parseArguments = (args: Array<string>): BmiValues => {
-  if (args.length < 4) throw new Error('Not enough arguments')
-  if (args.length > 4) throw new Error('Too many arguments')
-
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3]),
-    }
-  } else {
-    throw new Error('Provided values were not numbers!')
-  }
-}
 
 const calculateBmi = (height: number, weight: number): string => {
   const heightInMeters = height / 100
@@ -38,14 +25,31 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 }
 
-try {
-  const { height, weight } = parseArguments(process.argv)
-  console.log(calculateBmi(height, weight))
-} catch (error: unknown) {
-  let errorMessage = 'Something bad happened.'
-  if (error instanceof Error) {
-    let errorMessage = 'Something bad happened: '
-    errorMessage += ' Error: ' + error.message
+if (require.main === module) {
+  const parseArguments = (args: Array<string>): BmiValues => {
+    if (args.length < 4) throw new Error('Not enough arguments')
+    if (args.length > 4) throw new Error('Too many arguments')
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+      return {
+        height: Number(args[2]),
+        weight: Number(args[3]),
+      }
+    } else {
+      throw new Error('Provided values were not numbers!')
+    }
   }
-  console.log(errorMessage)
+  try {
+    const { height, weight } = parseArguments(process.argv)
+    console.log(calculateBmi(height, weight))
+  } catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+      let errorMessage = 'Something bad happened: '
+      errorMessage += ' Error: ' + error.message
+    }
+    console.log(errorMessage)
+  }
 }
+
+export default calculateBmi
--- part9/initEx/index.ts
+++ part9/initEx/index.ts
@@ -1,8 +1,25 @@
 import express from 'express'
+import calculateBmi from './bmiCalculator'
+
 const app = express()
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!')
+})
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query
+  if (!height || !weight || isNaN(Number(height)) || isNaN(Number(weight))) {
+    res.status(400).json({ error: 'malformatted parameters' })
+    return
+  }
+
+  const bmi = calculateBmi(Number(height), Number(weight))
+  res.json({
+    height: Number(height),
+    weight: Number(weight),
+    bmi,
+  })
 })
 
 const PORT = 3003
