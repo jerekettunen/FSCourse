e85f959fb55fc6291562770d0f8ee5ef228d13b7
jekettun <jereatkettunen@fmail.com>
2025-04-29 15:12:44 +0300

    added missing alert

--- part9/patientor/patientor/src/components/AddPatientEntryModal/NewEntryForm.tsx
+++ part9/patientor/patientor/src/components/AddPatientEntryModal/NewEntryForm.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-import { TextField, Button, Select, MenuItem, InputLabel, Slider } from '@mui/material';
+import { TextField, Button, Select, MenuItem, InputLabel, Slider, Alert } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers';
 import { Dayjs } from 'dayjs';
 import { Entry, NewEntry, Diagnosis, Patient } from '../../types';
 import patientService from '../../services/patients';
+import axios from 'axios';
 
 
 const newEntryFormStyle: React.CSSProperties = {
@@ -41,6 +42,7 @@ const NewEntryForm = ({ patient, diagnoses, setPatient }: { patient: Patient; di
   const [sickLeaveEndDate, setSickLeaveEndDate] = useState<Dayjs | null>(null);
   const [dischargeDate, setDischargeDate] = useState<Dayjs | null>(null);
   const [dischargeCriteria, setDischargeCriteria] = useState('');
+  const [error, setError] = useState('');
   // Add any additional state variables needed for the form
   const types = ['HealthCheck', 'OccupationalHealthcare', 'Hospital'] as const;
   // Handle form submission
@@ -96,14 +98,26 @@ const NewEntryForm = ({ patient, diagnoses, setPatient }: { patient: Patient; di
       console.error('Invalid entry type');
       return;
     }
-
-    console.log('Form submitted:', newEntry);
-    const result = await patientService.createEntry(patient.id, newEntry) as Entry;
-    const newPatient = {
+    try {
+      const result = await patientService.createEntry(patient.id, newEntry) as Entry;
+      const newPatient = {
       ...patient,
       entries: patient.entries.concat(result),
-    };
-    setPatient(newPatient);
+      };
+      setPatient(newPatient);
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      setError(`Error: ${error.response.data || 'Something went wrong'}`);
+      setTimeout(() => {
+        setError('');
+      }, 5000);
+    } else {
+      setError('Error: Something went wrong');
+      setTimeout(() => {
+        setError('');
+      }, 5000);
+    }
+  }
     // Reset form fields
     setEntryType('');
     setDescription('');
@@ -120,122 +134,136 @@ const NewEntryForm = ({ patient, diagnoses, setPatient }: { patient: Patient; di
   };
 
 
-  return (
-    <form onSubmit={handleSubmit} style={newEntryFormStyle}>
-      <InputLabel id="entry-type-label">Entry Type</InputLabel>
-      <Select
-        labelId="entry-type-label"
-        value={entryType}
-        onChange={({ target }) => setEntryType(target.value)}
-        fullWidth
-      >
-        <MenuItem value="" disabled>
-          Select entry type
-        </MenuItem>
-        {types.map((type) => (
-          <MenuItem key={type} value={type}>
-            {type}
-          </MenuItem>
-        ))}
-      </Select>
 
-      <TextField
-        label="Description"
-        fullWidth
-        multiline
-        value={description}
-        onChange={({ target }) => setDescription(target.value)}
-        />
-      <TextField
-        label="Specialist"
-        value={specialist}
-        onChange={({ target }) => setSpecialist(target.value)}
-        />
-      <DatePicker
-        label="Entry Date"
-        value={date}
-        onChange={(newValue) => setDate(newValue)}
-      />
-      <InputLabel id="diagnosis-codes-label">Diagnosis Codes</InputLabel>
-      <Select
-        labelId="diagnosis-codes-label"
-        multiple
-        value={diagnosisCodes}
-        onChange={({ target }) => setDiagnosisCodes(typeof target.value === 'string' ? target.value.split(',') : target.value)}
-        renderValue={(selected) => selected.join(', ')}
-        fullWidth
-      >
-        {diagnoses.map((diagnosis) => (
-          <MenuItem key={diagnosis.code} value={diagnosis.code}>
-            {diagnosis.code} - {diagnosis.name}
-          </MenuItem>
-        ))}
-      </Select>
-      {entryType === 'HealthCheck' && (
-        <div style={{ width: '95%', padding: '1rem 1.5rem' }}>
-          <InputLabel id="health-check-rating-label" style={{ marginBottom: '0.5rem' }}>
-            Health Check Rating
-          </InputLabel>
-          <Slider
-            value={healthCheckRating}
-            onChange={(_, newValue) => setHealthCheckRating(newValue as number)}
-            step={1}
-            marks={[
-              { value: 0, label: '0 - Healthy' },
-              { value: 1, label: '1 - Low Risk' },
-              { value: 2, label: '2 - High Risk' },
-              { value: 3, label: '3 - Critical Risk' },
-            ]}
-            min={0}
-            max={3}
-            valueLabelDisplay="auto"
-            style={{ margin: '0 auto', maxWidth: '90%' }}
-          />
+  return (
+    <div style={{ display: error === '' ? 'block' : 'none' }}>
+      {error && (
+        <div style={{ marginBottom: '1rem' }}>
+          <Alert severity="error">{error}</Alert>
         </div>
       )}
+      <form onSubmit={handleSubmit} style={newEntryFormStyle}>
+        <InputLabel id="entry-type-label">Entry Type</InputLabel>
+        <Select
+          labelId="entry-type-label"
+          value={entryType}
+          onChange={({ target }) => setEntryType(target.value)}
+          fullWidth
+        >
+          <MenuItem value="" disabled>
+            Select entry type
+          </MenuItem>
+          {types.map((type) => (
+            <MenuItem key={type} value={type}>
+              {type}
+            </MenuItem>
+          ))}
+        </Select>
 
-      {entryType === 'OccupationalHealthcare' && (
-        <>
-          <TextField
-            label="Employer Name"
-            fullWidth
-            value={employerName}
-            onChange={({ target }) => setEmployerName(target.value)}
-          />
-          <DatePicker
-            label="Sick Leave Start Date"
-            value={sickLeaveStartDate}
-            onChange={(newValue) => setSickLeaveStartDate(newValue)}
-          />
-          <DatePicker
-            label="Sick Leave End Date"
-            value={sickLeaveEndDate}
-            onChange={(newValue) => setSickLeaveEndDate(newValue)}
-          />
-        </>
-      )}
+        <TextField
+          label="Description"
+          fullWidth
+          multiline
+          value={description}
+          onChange={({ target }) => setDescription(target.value)}
+        />
+        <TextField
+          label="Specialist"
+          value={specialist}
+          onChange={({ target }) => setSpecialist(target.value)}
+        />
+        <DatePicker
+          label="Entry Date"
+          value={date}
+          onChange={(newValue) => setDate(newValue)}
+        />
+        <InputLabel id="diagnosis-codes-label">Diagnosis Codes</InputLabel>
+        <Select
+          labelId="diagnosis-codes-label"
+          multiple
+          value={diagnosisCodes}
+          onChange={({ target }) =>
+            setDiagnosisCodes(
+              typeof target.value === 'string' ? target.value.split(',') : target.value
+            )
+          }
+          renderValue={(selected) => selected.join(', ')}
+          fullWidth
+        >
+          {diagnoses.map((diagnosis) => (
+            <MenuItem key={diagnosis.code} value={diagnosis.code}>
+              {diagnosis.code} - {diagnosis.name}
+            </MenuItem>
+          ))}
+        </Select>
+        {entryType === 'HealthCheck' && (
+          <div style={{ width: '95%', padding: '1rem 1.5rem' }}>
+            <InputLabel
+              id="health-check-rating-label"
+              style={{ marginBottom: '0.5rem' }}
+            >
+              Health Check Rating
+            </InputLabel>
+            <Slider
+              value={healthCheckRating}
+              onChange={(_, newValue) => setHealthCheckRating(newValue as number)}
+              step={1}
+              marks={[
+                { value: 0, label: '0 - Healthy' },
+                { value: 1, label: '1 - Low Risk' },
+                { value: 2, label: '2 - High Risk' },
+                { value: 3, label: '3 - Critical Risk' },
+              ]}
+              min={0}
+              max={3}
+              valueLabelDisplay="auto"
+              style={{ margin: '0 auto', maxWidth: '90%' }}
+            />
+          </div>
+        )}
 
-      {entryType === 'Hospital' && (
-        <>
-          <DatePicker
-            label="Discharge Date"
-            value={dischargeDate}
-            onChange={(newValue) => setDischargeDate(newValue)}
-          />
-          <TextField
-            label="Discharge Criteria"
-            fullWidth
-            value={dischargeCriteria}
-            onChange={({ target }) => setDischargeCriteria(target.value)}
-          />
-        </>
-      )}
+        {entryType === 'OccupationalHealthcare' && (
+          <>
+            <TextField
+              label="Employer Name"
+              fullWidth
+              value={employerName}
+              onChange={({ target }) => setEmployerName(target.value)}
+            />
+            <DatePicker
+              label="Sick Leave Start Date"
+              value={sickLeaveStartDate}
+              onChange={(newValue) => setSickLeaveStartDate(newValue)}
+            />
+            <DatePicker
+              label="Sick Leave End Date"
+              value={sickLeaveEndDate}
+              onChange={(newValue) => setSickLeaveEndDate(newValue)}
+            />
+          </>
+        )}
 
-      <Button type="submit" variant="contained" color="primary">
-        Submit
-      </Button>
-  
-    </form>
+        {entryType === 'Hospital' && (
+          <>
+            <DatePicker
+              label="Discharge Date"
+              value={dischargeDate}
+              onChange={(newValue) => setDischargeDate(newValue)}
+            />
+            <TextField
+              label="Discharge Criteria"
+              fullWidth
+              value={dischargeCriteria}
+              onChange={({ target }) => setDischargeCriteria(target.value)}
+            />
+          </>
+        )}
+
+        <Button type="submit" variant="contained" color="primary">
+          Submit
+        </Button>
+      </form>
+    </div>
   );
 };
 export default NewEntryForm;
