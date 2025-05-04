f36729b507eead04e48a681188f467da42cee8e8
jerekettunen <jereatkettunen@gmail.com>
2025-05-04 13:40:20 +0300

    up to 13.4

--- part13/backend/index.js
+++ part13/backend/index.js
@@ -1,21 +1,91 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
 
-console.log('DATABASE_URL', process.env.DATABASE_URL)
+app.use(express.json())
+
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
 })
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    console.log(blogs)
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
-  }
-}
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+Blog.sync()
 
-main()
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.get('/api/blogs/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const blog = await Blog.findByPk(id)
+    if (blog) {
+      return res.json(blog)
+    } else {
+      return res.status(404).json({ error: 'Blog not found' })
+    }
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+app.post('/api/blogs', async (req, res) => {
+  console.log('req.body', req.body)
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const blog = await Blog.destroy({ where: { id } })
+    if (blog) {
+      return res.status(204).end()
+    } else {
+      return res.status(404).json({ error: 'Blog not found' })
+    }
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ part13/backend/models/blog.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require('sequelize')
+
+class Note extends Model {}
+Note.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  important: {
+    type: DataTypes.BOOLEAN
+  },
+  date: {
+    type: DataTypes.DATE
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'note'
+})
+
